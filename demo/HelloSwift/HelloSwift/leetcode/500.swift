3264f8510a7b67b7a16d29045d012cca5f334780
muhlenXi <xiyinjun_545@163.com>
2019-09-28 00:00:10 +0800

    Add leetcode 500

---
+++ demo/HelloSwift/HelloSwift/leetcode/500.swift
@@ -0,0 +1,43 @@
+//
+//  500.swift
+//  HelloSwift
+//
+//  Created by muhlenXi on 2019/9/27.
+//  Copyright © 2019 muhlenXi. All rights reserved.
+//
+
+import Foundation
+
+func findWords(_ words: [String]) -> [String] {
+    let dictionary = makeAlphabetDictionary()
+    var results = [String]()
+    
+    for element in words {
+        let lowercased = element.lowercased()
+        let firstValue = dictionary[String(lowercased.first!)]!
+        var sum = 0
+        for character in lowercased {
+            sum  += dictionary[String(character)]!
+        }
+        if sum == firstValue * element.count {
+            results.append(element)
+        } else {
+            continue
+        }
+    }
+    
+    return results
+}
+
+func makeAlphabetDictionary() -> [String: Int] {
+    let firstRows = [["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],["a", "s", "d", "f", "g", "h", "j", "k", "l"],["z", "x", "c", "v", "b", "n", "m"]]
+    var dictionary = [String: Int]()
+    
+    for (index, row) in firstRows.enumerated() {
+        for element in row {
+            dictionary[element] = index+1
+        }
+    }
+    
+    return dictionary
+}
