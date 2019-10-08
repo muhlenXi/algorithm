49700013138a55f487feac2213d9a26697624341
muhlenXi <xiyinjun_545@163.com>
2019-10-08 22:49:36 +0800

    add leetcode 1

--- demo/HelloSwift/HelloSwift/main.swift
+++ demo/HelloSwift/HelloSwift/main.swift
@@ -8,47 +8,19 @@
 
 import Foundation
 
-//let node0 = ListNode(1)
-//let node1 = ListNode(2)
-//let node2 = ListNode(3)
-//let node3 = ListNode(4)
-//let node4 = ListNode(5)
-//let node5 = ListNode(6)
-//let node6 = ListNode(4)
-//
-//node0.next = node1
-//node1.next = node2
-//node2.next = node3
-//node3.next = node4
-//
-//node4.next = node5
-//node5.next = node6
+// MARK: - leetcode 1
 
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var map = [Int: Int]()
+    for (index, element) in nums.enumerated() {
+        if let index0 = map[target-element] {
+            return [index0, index]
+        } else {
+            map[element] = index
+        }
+    }
+    
+    return [Int]()
+}
 
-
-var node0 = TreeNode(5)
-var node1 = TreeNode(8)
-var node2 = TreeNode(5)
-
-var node3 = TreeNode(5)
-var node4 = TreeNode(7)
-var node5 = TreeNode(5)
-var node6 = TreeNode(2)
-
-node0.left = node1
-node0.right = node2
-
-//node2.left = node3
-//node2.right = node4
-
-//node1.left = node3
-//node1.right = node4
-//
-//node2.left = node5
-//node2.right = node6
-
-
-
-
-let text = "ballon"
-print(maxNumberOfBalloons(text))
+print(twoSum([2, 7, 11, 15], 9))
