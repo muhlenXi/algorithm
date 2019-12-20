a41a0e223d2d8261bed250b11b2517d82ed3d3f8
xiyinjun <xiyinjun_545@163.com>
2019-12-20 20:06:39 +0800

    Add 58,66,67,118,119,292

--- demo/HelloSwift/HelloSwift/main.swift
+++ demo/HelloSwift/HelloSwift/main.swift
@@ -8,43 +8,4 @@
 
 import Foundation
 
-func countAndSay(_ n: Int) -> String {
-    if n == 1 {
-        return "1"
-    }
-    
-    var output = "1"
-    for _ in 1..<n {
-        say(&output)
-    }
-    return output
-}
-
-func say(_ text: inout String){
-    let pattern = text
-    var counts = 0
-    text = ""
-    var repeatCharacter = pattern.first!
-    for element in pattern {
-        if element == repeatCharacter {
-            counts += 1
-        } else {
-            text.append("\(counts)")
-            text.append(repeatCharacter)
-            repeatCharacter = element
-            counts = 1
-        }
-    }
-    
-    text.append("\(counts)")
-    text.append(repeatCharacter)
-}
-
-print(countAndSay(1))
-print(countAndSay(2))
-print(countAndSay(3))
-print(countAndSay(4))
-print(countAndSay(5))
-print(countAndSay(6))
-print(countAndSay(7))
 
