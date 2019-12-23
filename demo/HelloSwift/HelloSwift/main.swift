d1aaab9a02fcb4d3db8d027f9461fbaffa686716
muhlenXi <xiyinjun_545@163.com>
2019-12-23 08:23:35 +0800

    update swift

--- demo/HelloSwift/HelloSwift/main.swift
+++ demo/HelloSwift/HelloSwift/main.swift
@@ -8,34 +8,3 @@
 
 import Foundation
 
-func findSpecialInteger(_ arr: [Int]) -> Int {
-    guard arr.count != 1 else {
-        return arr.first!
-    }
-
-    var target = arr.count / 4
-    if arr.count % 4 != 0 {
-        target += 1
-    }
-    
-    for index in 0..<arr.count {
-        if arr[index] == arr[index+target] {
-            return arr[index]
-        }
-    }
-    
-    return 0
-}
-
-let list = [1, 1]
-print(findSpecialInteger(list))
-
-
-
-
-
-
-
-
-
-
