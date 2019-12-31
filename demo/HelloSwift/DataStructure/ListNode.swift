bb8a7873a263a8935cb63c56793421fa8ffab902
xiyinjun <xiyinjun_545@163.com>
2019-12-31 19:28:00 +0800

    Add LC 2019-12-31

--- demo/HelloSwift/DataStructure/ListNode.swift
+++ demo/HelloSwift/DataStructure/ListNode.swift
@@ -25,3 +25,125 @@ func printList(_ head: ListNode?) {
         currentNode = currentNode!.next
     }
 }
+
+class Traversal {
+    // 144
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        var ret = [Int]()
+        preorderTraversalLoop(&ret, root)
+        return ret
+    }
+
+    func preorderTraversalRecursion(_ result: inout [Int], _ node: TreeNode?) {
+        guard let n = node else {
+            return
+        }
+        result.append(n.val)
+        preorderTraversalRecursion(&result, n.left)
+        preorderTraversalRecursion(&result, n.right)
+    }
+
+    func preorderTraversalLoop(_ result: inout [Int], _ node: TreeNode?) {
+        guard let root = node else {
+            return
+        }
+        
+        var stack = [TreeNode]()
+        var node: TreeNode? = root
+        
+        while !stack.isEmpty || node != nil {
+            while node != nil {
+                stack.append(node!)
+                result.append(node!.val)
+                node = node?.left
+            }
+            
+            let cur = stack.removeLast()
+            node = cur.right
+        }
+    }
+
+    // 94
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        var ret = [Int]()
+        inorderTraversalLoop(&ret, root)
+        return ret
+    }
+
+    func inorderTraversalLoop(_ result: inout [Int], _ node: TreeNode?) {
+        guard let n = node else {
+            return
+        }
+        
+        var stack = [TreeNode]()
+        var node: TreeNode? = n
+        
+        while !stack.isEmpty || node != nil {
+            if node != nil {
+                stack.append(node!)
+                node = node?.left
+            } else {
+                let n = stack.removeLast()
+                result.append(n.val)
+                
+                if n.right != nil {
+                    node = n.right
+                }
+            }
+        }
+    }
+
+    func inorderTraversalRecursion(_ result: inout [Int], _ node: TreeNode?) {
+        guard let n = node else {
+            return
+        }
+        
+        inorderTraversalRecursion(&result, n.left)
+        result.append(n.val)
+        inorderTraversalRecursion(&result, n.right)
+    }
+
+    // 145
+    func postorderTraversal(_ root: TreeNode?) -> [Int] {
+        var ret = [Int]()
+        postorderTraversalLoop(&ret, root)
+        return ret
+    }
+
+    func postorderTraversalLoop(_ result: inout [Int], _ node: TreeNode?) {
+        guard let n = node else {
+            return
+        }
+        
+        var stack = [TreeNode]()
+        var node: TreeNode? = n
+        var flag: TreeNode?
+        
+        while !stack.isEmpty || node != nil {
+            if node != nil {
+                stack.append(node!)
+                node = node?.left
+            } else {
+                let top = stack.last!
+                if top.right == nil || (flag != nil && flag!.val == top.right!.val) {
+                    result.append(top.val)
+                    stack.removeLast()
+                    flag = top
+                    node = nil
+                } else {
+                    node = top.right
+                }
+            }
+        }
+    }
+
+    func postorderTraversalRecursion(_ result: inout [Int], _ node: TreeNode?) {
+        guard let n = node else {
+            return
+        }
+        
+        postorderTraversalRecursion(&result, n.left)
+        postorderTraversalRecursion(&result, n.right)
+        result.append(n.val)
+    }
+}
--- demo/HelloSwift/HelloSwift/main.swift
+++ demo/HelloSwift/HelloSwift/main.swift
@@ -55,35 +55,6 @@ let h5 = URL(string:"https://www.fazzaco.com/h5/getCompanyDetailA?languageType=0
 openDetailSceneByURL(scheme)
 openDetailSceneByURL(h5)
 
-// 82
-func deleteDuplicates(_ head: ListNode?) -> ListNode? {
-    let top = ListNode(110)
-    top.next = head
-    
-    var current: ListNode? = top
-    
-    while current != nil {
-        if current?.next != nil {
-            let val = current!.next!.val
-            var count = 0
-            var node = current!.next
-            while node != nil && node!.val == val{
-                count += 1
-                node = node?.next
-            }
-            if count == 1 {
-                current = current?.next
-            } else {
-                current?.next = node
-            }
-        } else {
-            current = current?.next
-        }
-    }
-    
-    return top.next
-}
-
 let node1 = ListNode(1)
 let node2 = ListNode(2)
 let node3 = ListNode(3)
@@ -99,8 +70,28 @@ node4.next = node5
 node5.next = node6
 node6.next = node7
 
-let h = deleteDuplicates(node1)
-printList(h)
+
+
+let t1 = TreeNode(1)
+let t2 = TreeNode(2)
+let t3 = TreeNode(3)
+let t4 = TreeNode(4)
+let t5 = TreeNode(5)
+let t6 = TreeNode(6)
+let t7 = TreeNode(7)
+let t8 = TreeNode(8)
+
+t1.left = t2
+t1.right = t3
+t2.left = t4
+t2.right = t5
+t3.left = t6
+t3.right = t7
+//t7.left = t8
+
+
+
+
 
 
 
