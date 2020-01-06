e13dd483fd1500888c191b1c4a2e6e377dd96fa4
xiyinjun <xiyinjun_545@163.com>
2020-01-06 11:32:38 +0800

    Update Java

--- demo/HelloJava/src/Hello.java
+++ demo/HelloJava/src/Hello.java
@@ -8,6 +8,21 @@ public class Hello {
 
     }
 
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode nodeA = headA;
+        ListNode nodeB = headB;
+        while(nodeA != nodeB) {
+            nodeA = nodeA == null ? headB : nodeA.next;
+            nodeB = nodeB == null ? headA : nodeB.next;
+        }
+
+        return nodeA;
+    }
+
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
