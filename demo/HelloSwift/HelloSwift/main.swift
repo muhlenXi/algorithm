c38408b173ce86552a6805eb974c91b4402c2a50
xiyinjun <xiyinjun_545@163.com>
2020-01-07 21:22:48 +0800

    Update files

--- demo/HelloSwift/HelloSwift/main.swift
+++ demo/HelloSwift/HelloSwift/main.swift
@@ -89,29 +89,23 @@ openDetailSceneByURL(h5)
 
 
 
-
-
-
-
-
-let array = [10,5,-3,3,2,nil,11,3,-2,nil,1]
-
-
-let t1: TreeNode? = TreeNode(1)
-let t2: TreeNode? = TreeNode(2)
-let t3: TreeNode? = TreeNode(3)
-let t4: TreeNode? = TreeNode(4)
-let t5: TreeNode? = TreeNode(5)
-let t6: TreeNode? = TreeNode(6)
-let t7: TreeNode? = TreeNode(7)
-let t8: TreeNode? = TreeNode(3)
-let t9: TreeNode? = TreeNode(-2)
-let t10: TreeNode? = nil
-let t11: TreeNode? = TreeNode(1)
-let t12: TreeNode? = TreeNode(12)
+// [15,9,21,7,13,19,23,5,null,11,null,17]
+let t1: TreeNode? = TreeNode(15)
+let t2: TreeNode? = TreeNode(9)
+let t3: TreeNode? = TreeNode(21)
+let t4: TreeNode? = TreeNode(7)
+let t5: TreeNode? = TreeNode(13)
+let t6: TreeNode? = TreeNode(19)
+let t7: TreeNode? = TreeNode(23)
+let t8: TreeNode? = TreeNode(5)
+let t9: TreeNode? = nil
+let t10: TreeNode? = TreeNode(11)
+let t11: TreeNode? = nil
+let t12: TreeNode? = TreeNode(17)
 let t13: TreeNode? = TreeNode(13)
 let t14: TreeNode? = TreeNode(14)
 let t15: TreeNode? = TreeNode(15)
+
 
 t1?.left = t2
 t1?.right = t3
@@ -119,56 +113,20 @@ t2?.left = t4
 t2?.right = t5
 t3?.left = t6
 t3?.right = t7
-//t4?.left = t8
-//t4?.right = t9
-//t5?.left = t10
-//t5?.right = t11
-//t6?.left = t12
+t4?.left = t8
+t4?.right = t9
+t5?.left = t10
+t5?.right = t11
+t6?.left = t12
 //t6?.right = t13
 //t7?.left = t14
 //t7?.right = t15
 
-// 113
-func pathSum(_ root: TreeNode?, _ sum: Int) -> [[Int]] {
-    var ret = [[Int]]()
-    guard root != nil else {
-        return ret
-    }
-    
-    
-    
-    return [[Int]]()
-}
 
-func binaryTreePaths(_ root: TreeNode?) -> [String] {
-    guard let node = root else {
-        return [String]()
-    }
-    
-    if node.left == nil, node.right == nil {
-        return ["\(node.val)"]
-    }
-    
-    var strings = [String]()
-    
-    for element in binaryTreePaths(node.left) {
-        var total = [String]()
-        total.append("\(node.val)")
-        total.append(element)
-        strings.append(total.joined(separator: "->"))
-    }
-    
-    for element in binaryTreePaths(node.right){
-        var total = [String]()
-        total.append("\(node.val)")
-        total.append(element)
-        strings.append(total.joined(separator: "->"))
-    }
-    
-    return strings
-}
 
-print(binaryTreePaths(t1))
+
+
+
 
 
 
