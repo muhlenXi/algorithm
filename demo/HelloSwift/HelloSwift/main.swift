4002246c3fcaa2553228121cc3ed21adec9c3ddd
xiyinjun <xiyinjun_545@163.com>
2020-01-14 13:52:43 +0800

    Add LC 2020-01-14

--- demo/HelloSwift/HelloSwift/main.swift
+++ demo/HelloSwift/HelloSwift/main.swift
@@ -9,33 +9,34 @@
 import Foundation
 
 
-func largestTriangleArea(_ points: [[Int]]) -> Double {
-    var sMax = Double(0)
-    var i = 0
-    while i < points.count {
-        var j = i + 1
-        while j < points.count {
-            var z = j + 1
-            while z < points.count {
-                let p0 = points[i]
-                let p1 = points[j]
-                let p2 = points[z]
-                
-                // s = 1/2 * ((x1-x0)(y2-y0) - (x2-x0)(y1-y0))
-                var s = abs(Double(p1[0]-p0[0])*Double(p2[1]-p0[1]) - Double(p2[0]-p0[0])*Double(p1[1]-p0[1]))
-                s *= Double(0.5)
-
-                sMax = max(s, sMax)
-                z += 1
-            }
-            j += 1
-        }
-        i += 1
-    }
-    return sMax
+func permute(_ nums: [Int]) -> [[Int]] {
+    var result = [[Int]]()
+    
+    return result
 }
 
-print(largestTriangleArea([[35,-23],[-12,-48],[-34,-40],[21,-25],[-35,-44],[24,1],[16,-9],[41,4],[-36,-49],[42,-49],[-37,-20],[-35,11],[-2,-36],[18,21],[18,8],[-24,14],[-23,-11],[-8,44],[-19,-3],[0,-10],[-21,-4],[23,18],[20,11],[-42,24],[6,-19]]))
+// 885
+func spiralMatrixIII(_ R: Int, _ C: Int, _ r0: Int, _ c0: Int) -> [[Int]] {
+    var points = [[Int]]()
+    
+    var row = r0
+    var column = c0
+    while column < C {
+        points.append([row, column])
+        column += 1
+    }
+    row += 1
+    
+    
+    return points
+}
+
+var R = 5
+var C = 6
+var r0 = 1
+var c0 = 4
+
+print(spiralMatrixIII(R, C, r0, c0))
 
 
 
@@ -65,17 +66,13 @@ print(largestTriangleArea([[35,-23],[-12,-48],[-34,-40],[21,-25],[-35,-44],[24,1
 
 
 
-
-
-
-// [1,0,48,null,null,12,49,null,null,null,null]
 let t1: TreeNode? = TreeNode(1)
-let t2: TreeNode? = TreeNode(0)
-let t3: TreeNode? = TreeNode(48)
-let t4: TreeNode? = nil
-let t5: TreeNode? = nil
-let t6: TreeNode? = TreeNode(12)
-let t7: TreeNode? = TreeNode(49)
+let t2: TreeNode? = TreeNode(2)
+let t3: TreeNode? = TreeNode(3)
+let t4: TreeNode? = TreeNode(4)
+let t5: TreeNode? = TreeNode(5)
+let t6: TreeNode? = TreeNode(6)
+let t7: TreeNode? = TreeNode(7)
 let t8: TreeNode? = TreeNode(8)
 let t9: TreeNode? = TreeNode(9)
 let t10: TreeNode? = TreeNode(10)
@@ -86,20 +83,24 @@ let t14: TreeNode? = TreeNode(14)
 let t15: TreeNode? = TreeNode(15)
 
 
-//t1?.left = t2
-//t1?.right = t3
-//t2?.left = t4
-//t2?.right = t5
-//t3?.left = t6
-//t3?.right = t7
-//t4?.left = t8
-//t4?.right = t9
-//t5?.left = t10
-//t5?.right = t11
-//t6?.left = t12
-//t6?.right = t13
-//t7?.left = t14
-//t7?.right = t15
+t1?.left = t2
+t1?.right = t3
+t2?.left = t4
+t2?.right = t5
+t3?.left = t6
+t3?.right = t7
+t4?.left = t8
+t4?.right = t9
+t5?.left = t10
+t5?.right = t11
+t6?.left = t12
+t6?.right = t13
+t7?.left = t14
+t7?.right = t15
+
+
+
+
 
 
 
