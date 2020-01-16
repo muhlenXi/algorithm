d2a898643cafedc28a66b72a72537d9ae6ecf855
xiyinjun <xiyinjun_545@163.com>
2020-01-16 21:14:00 +0800

    Add LC 2020-01-16

--- demo/HelloSwift/HelloSwift/main.swift
+++ demo/HelloSwift/HelloSwift/main.swift
@@ -9,6 +9,98 @@
 import Foundation
 
 
+// 47
+func permuteUnique(_ nums: [Int]) -> [[Int]] {
+    var track = [Int]()
+    var result = [[Int]]()
+    backtrack(nums, &track, &result)
+    return result
+}
+
+func backtrack(_ nums: [Int], _ tracks: inout [Int], _ result: inout [[Int]]) {
+    if nums.count == tracks.count {
+        result.append(tracks)
+        return
+    }
+    for element in nums {
+        if tracks.contains(element) {
+            continue
+        }
+        
+        tracks.append(element)
+        backtrack(nums, &tracks, &result)
+        tracks.removeLast()
+    }
+}
+
+let array = [1,1,2]
+print(permuteUnique(array))
+
+
+
+
+func queen(_ n: Int) {
+    var board = [[String]]()
+    for _ in 0..<n {
+        board.append(Array(repeating: ".", count: n))
+    }
+    bt(&board, 0)
+    print(board)
+}
+
+func bt(_ board: inout [[String]], _ row: Int) {
+    if row == board.count {
+        print(board)
+        return
+    }
+    
+    let columns = board[row].count
+    for col in 0..<columns {
+        if isValid(board, row, col) == false {
+            continue
+        }
+        
+        board[row][col] = "Q"
+        bt(&board, row+1)
+        board[row][col] = "."
+    }
+}
+
+func isValid(_ board: [[String]], _ row: Int, _ cloumn: Int) -> Bool {
+    let length = board.count
+    for r in 0..<length {
+        if board[r][cloumn] == "Q" {
+            return false
+        }
+    }
+    
+    var i = row - 1
+    var j = cloumn + 1
+    while i >= 0 && j < length {
+        if board[i][j] == "Q" {
+            return false
+        }
+        i -= 1
+        j += 1
+    }
+    
+    var ii = row - 1
+    var jj = cloumn - 1
+    while ii >= 0 && jj >= 0 {
+        if board[ii][jj] == "Q" {
+            return false
+        }
+        ii -= 1
+        jj -= 1
+    }
+    
+    return true
+}
+
+let n = 4
+//queen(n)
+
+
 
 
 
