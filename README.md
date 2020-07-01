612cd7c90a7d567a8c84790f3267598ab20c90f2
xiyinjun <xiyinjun_545@163.com>
2020-07-01 20:40:08 +0800

    Add 322 718

--- README.md
+++ README.md
@@ -7,6 +7,11 @@
 - [1. two sum](./lc-solution/1.md)
 - [27. Remove Element](./lc-solution/27.md)
 
+## Dynamic Programming
+
+- [322. Coin Change](./lc-solution/322.md)
+- [718. Maximum Length of Repeated Subarray](./lc-solution/718.md)
+
 ## 剑指 offer
 
 - [9. 用两个栈实现队列](./sword-offer/9.md)
--- demo/HelloPython/main.py
+++ demo/HelloPython/main.py
@@ -1,33 +1,3 @@
 from typing import List
 
-# class Solution:
-
-class CQueue:
-    stack1 = list()
-    stack2 = list()
-
-    def __init__(self):
-        return
-
-    def appendTail(self, value: int) -> None:
-        self.stack1.append(value)
-
-    def deleteHead(self) -> int:
-        if self.stack2:
-            return self.stack2.pop()
-        else:
-            if self.stack1:
-                while self.stack1:
-                    self.stack2.append(self.stack1.pop())
-                return self.stack2.pop()
-            else:
-                return -1
-
-action = ["appendTail","deleteHead","appendTail","appendTail","deleteHead","deleteHead","appendTail","appendTail","appendTail","deleteHead","deleteHead","deleteHead","appendTail","appendTail","deleteHead","deleteHead","deleteHead","appendTail","appendTail","deleteHead"]
-values = [[11],[],[1],[17],[],[],[19],[20],[13],[],[],[],[12],[3],[],[],[],[10],[19],[]]
-c = CQueue()
-for index in range(len(action)):
-    if action[index] == "appendTail":
-        print(c.appendTail(values[index][0]))
-    elif action[index] == "deleteHead":
-        print(c.deleteHead())
+class Solution:
