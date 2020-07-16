523f44b7e0a8f9eea13a06379583f98518a2fb12
xiyinjun <xiyinjun_545@163.com>
2020-07-16 21:12:00 +0800

    Add 785

--- demo/HelloPython/main.py
+++ demo/HelloPython/main.py
@@ -1,4 +1,5 @@
 from typing import List
+import collections
 
 class TreeNode:
     def __init__(self, x):
@@ -7,38 +8,35 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        if len(nums1) == 0 or len(nums2) == 0:
-            return []
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        count = len(graph)
+        UNCOLORED = 0
+        RED = 1
+        GREEN = 2
+        color = [UNCOLORED] * count
+        for i in range(count):
+            if color[i] == UNCOLORED:
+                q = collections.deque([i])
+                color[i] = RED
+                while q:
+                    node = q.popleft()
+                    colorNext = (GREEN if color[node] == RED else RED)
+                    for neighbor in graph[node]:
+                        if color[neighbor] == UNCOLORED:
+                            q.append(neighbor)
+                            color[neighbor] = colorNext
+                        elif color[neighbor] != colorNext:
+                            return False
 
-        n1 = sorted(nums1)
-        n2 = sorted(nums2)
-        i1 = 0
-        i2 = 0
-        common = []
-        while i1 < len(n1) and i2 < len(n2):
-            if n1[i1] == n2[i2]:
-                common.append(n1[i1])
-                i1 += 1
-                i2 += 1
-            elif n1[i1] > n2[i2]:
-                i2 += 1
-            else:
-                i1 += 1
-        return common
-
-
-
-
+        return True
 
 
 
 
 
 solution = Solution()
-list1 = [1, 2, 2, 1, 5, 4]
-list2 = [4, 5]
-print(solution.intersect(list1, list2))
+graph = [[1, 3], [0, 2], [1, 3], [0, 2]]
+print(solution.isBipartite(graph))
 
 
 
@@ -48,19 +46,3 @@ print(solution.intersect(list1, list2))
 
 
 
-
-
-
-t1 = TreeNode(1)
-t2 = TreeNode(2)
-t3 = TreeNode(3)
-t4 = TreeNode(4)
-t5 = TreeNode(5)
-t6 = TreeNode(6)
-t7 = TreeNode(7)
-t1.left = t2
-t1.right = t3
-t2.left = t4
-t2.right = t5
-t3.left = t6
-t3.right = t7
