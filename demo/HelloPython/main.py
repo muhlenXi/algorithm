4870476665d4aab230c285479eb66428c9ec5c9a
xiyinjun <xiyinjun_545@163.com>
2020-07-22 20:54:28 +0800

    Add 153 154

--- demo/HelloPython/main.py
+++ demo/HelloPython/main.py
@@ -8,31 +8,10 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    # 95
-    def generateTrees(self, n: int) -> List[TreeNode]:
-        if n <= 0:
-            return []
-        return self.generate(1, n)
-
-
-    def generate(self, start: int, end: int) -> List[TreeNode]:
-        if start > end:
-            return [None]
-        list = []
-        for i in range(start, end+1):
-            lefts = self.generate(start, i - 1)
-            rights = self.generate(i + 1, end)
-            for left in lefts:
-                for right in rights:
-                    node = TreeNode(i)
-                    node.left = left
-                    node.right = right
-                    list.append(node)
-
-        return list
 
 
 
-n = 3
+
+
+
 solution = Solution()
-print(solution.generateTrees(n))
