38029258d634cde237177944cb48ce9b16c89ec7
xiyinjun <xiyinjun_545@163.com>
2020-08-10 20:45:49 +0800

    update 206

--- demo/HelloSwift/HelloSwift/main.swift
+++ demo/HelloSwift/HelloSwift/main.swift
@@ -8,48 +8,24 @@
 
 import Foundation
 
-let node1 = ListNode(1)
-let node2 = ListNode(2)
-let node3 = ListNode(3)
-
-node1.next = node2
-node2.next = node3
-
-
-
-func reverseList(_ head: ListNode?) -> ListNode? {
-    if head == nil || head?.next == nil {
-        return head
-    }
+protocol MyProtocol {
     
-    let next = reverseList(head?.next)
-    
-    head?.next?.next = head
-    head?.next = nil
-    
-    return next
 }
-
-func reverseList1(_ head: ListNode?) -> ListNode? {
-    var pre: ListNode? = nil
-    var current: ListNode? = head
-    var next: ListNode? = current?.next
-    
-    while current != nil {
-        current?.next = pre
-        pre = current
-        
-        current = next
-        next = current?.next
-    }
-    
-    return pre
+struct MyStruct {
 }
-
-//let n = reverseList(node1)
-let n1 = reverseList1(node1)
-
-//printList(n)
-printList(n1)
-
-
+extension MyStruct: MyProtocol {
+    func extensionMethod() {
+        print("结构体")
+    }
+}
+extension MyProtocol {
+    func extensionMethod() {
+        print("协议")
+    }
+}
+ 
+let myStruct = MyStruct()
+let proto: MyProtocol = myStruct
+ 
+myStruct.extensionMethod() // -> “结构体”
+proto.extensionMethod() // -> “协议”
